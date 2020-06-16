679e0dac63b3857e25164a14e9ee3ad0c0ebd94d
Rohit Macherla <rohit.macherla2@sky.uk>
2020-06-16 15:46:37 +0100

    using generators to go through each note

--- force-app/main/default/lwc/notesList/notesList.js
+++ force-app/main/default/lwc/notesList/notesList.js
@@ -1,5 +1,21 @@
 import { LightningElement, api } from 'lwc';
 
 export default class NotesList extends LightningElement {
-    @api notes;
+    @api notesiterator;
+    note;
+    done;
+
+    connectedCallback() {
+        // initialize component
+        this.note = this.notesiterator.next().value;
+    }
+
+    getNextNote() {
+        if(this.done)
+            return;
+        const nextItem = this.notesiterator.next();
+        this.note = nextItem.value;
+        this.done = nextItem.done;
+        console.log(`${this.done}`);
+    }
 }
--- force-app/main/default/lwc/testNotes/testNotes.js
+++ force-app/main/default/lwc/testNotes/testNotes.js
@@ -1,12 +1,6 @@
 import { LightningElement } from 'lwc';
+import { notesGenerator } from './notesIterator';
 
 export default class TestNotes extends LightningElement {
-    notes = [
-        {
-            description: "I went to the woods because I wished to live deliberately, to front only the essential facts of life, and see if I could not learn what it had to teach, and not, when I came to die, discover that I had not lived."
-        },
-        {
-            description: "test"
-        }
-    ]
+    notesGenerator = notesGenerator();
 }
